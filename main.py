5bb7e57b511166527dc01706180233e69ad1f90f
Nicolas POZIN <nicolas@habx.fr>
2019-09-21 23:51:56 +0200

    time measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 from blob_detector import Pixel, Blob
 from image_generator import generate_image
@@ -7,6 +8,9 @@ if __name__ == '__main__':
 
     # generate synthetic image
     image = generate_image()
+
+    # time measure
+    to = time.time()
 
     # detect every blobs in the image
     list_blobs = []
@@ -20,10 +24,10 @@ if __name__ == '__main__':
             pixel = Pixel(x=pix[0], y=pix[1], image=image)
             if pixel.value != 1:
                 continue
-            blob = Blob(image, index=p+2, seed_pixel=pixel, area=0)
+            blob = Blob(image, index=p + 2, seed_pixel=pixel, area=0)
             blob.grow(pixel)
             if blob.area > 400:
                 list_blobs.append(blob)
 
     print("number of blobs detected ", len(list_blobs))
-
+    print("process time", time.time() - to)
