95d6447b399e05bc7801fad76f177c08631548f0
Nicolas POZIN <nicolas@habx.fr>
2019-09-21 22:48:48 +0200

    main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from blob_detector.py import Pixel, Blob
-from image_generator.py import generate_image
+from blob_detector import Pixel, Blob
+from image_generator import generate_image
 
 if __name__ == '__main__':
 
@@ -9,29 +9,23 @@ if __name__ == '__main__':
     image = generate_image()
 
     # detect every blobs in the image
-    non_zeros = np.where(im == 1)
+    list_blobs = []
+    non_zeros = np.where(image == 1)
     if not non_zeros:
         print("no blob")
     else:
         non_zero_pixels = list(
             zip(non_zeros[0], non_zeros[1]))  # list of tuples with non zeros pixel coords
-        for pix in non_zero_pixels:
-            pixel=Pixel()
-            if image[pix[0], pix[1]] != 1:
+        for p, pix in enumerate(non_zero_pixels):
+            pixel = Pixel(x=pix[0], y=pix[1], image=image)
+            if pixel.value != 1:
                 continue
-            blob_grow(el[0], el[1], im, ind)
-            nb_blobs += 1
+            blob = Blob(image, index=p, area=0)
+            blob.grow(pixel)
+            if blob.area > 400:
+                list_blobs.append(blob)
 
-    def image_loop(im):
-        result = np.where(im == 1)
-        ind = 2
-        if not result:
-            return ind - 2
-        listOfCoordinates = list(zip(result[0], result[1]))
-        nb_blobs = 0
-        for el in listOfCoordinates:
-            if im[el[0], el[1]] != 1:
-                continue
-            blob_grow(el[0], el[1], im, ind)
-            nb_blobs += 1
-        return nb_blobs
+    print("number of blobs detected ", len(list_blobs))
+
+    for blob in list_blobs:
+        print(blob)
