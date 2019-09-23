9f3c222523e9b55edbfa05a1498204535fe35a8d
Nicolas POZIN <nicolas@habx.fr>
2019-09-23 17:29:29 +0200

    add test module

---
+++ blob_detector_test.py
@@ -0,0 +1,16 @@
+# test module
+
+import pytest
+from blob_detector import BlobDetector
+
+
+def blob_detector_test():
+    """
+    Test : blobs.tif shall contain 51 blobs of intensiey = 255 and size >=400
+    :return:
+    """
+
+    blob_detector = BlobDetector(image_path='blobs.tif', blob_size=400, blob_intensity=255)
+    list_detected_blobs = blob_detector.apply()
+
+    assert len(list_detected_blobs) == 51
