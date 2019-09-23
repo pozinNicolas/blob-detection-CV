2f6dc88aea0056fb8c93885b947f89b47d57198f
Nicolas POZIN <nicolas@habx.fr>
2019-09-23 18:24:29 +0200

    add parser

--- main.py
+++ main.py
@@ -8,10 +8,30 @@ from blob_detector import BlobDetector
 import time
 
 if __name__ == '__main__':
+
+    ######### parser #########
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--image_path", help="image path",
+                        default='data/blobs.tif')
+    parser.add_argument("-bs", "--blob_size", help="blob size",
+                        default='400')
+    parser.add_argument("-bi", "--blob_intensity", help="blob intensity",
+                        default='255')
+    args = parser.parse_args()
+
+    image_path = str(args.image_path)
+    blob_size = int(args.blob_size)
+    blob_intensity = int(args.blob_intensity)
+
+
+    ######### blob detection #########
     to = time.time()
 
     # detects all blobs with given size and intensity in a tif image
-    blob_detector = BlobDetector(image_path='data/blobs.tif', blob_size=400, blob_intensity=255)
+    blob_detector = BlobDetector(image_path='data/blobs.tif', blob_size=blob_size,
+                                 blob_intensity=blob_intensity)
     list_detected_blobs = blob_detector.apply()
 
     print("blob detection process time", time.time() - to)
