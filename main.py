51e8d898fac4a09fd5287b72393d59a7bde99bcb
Nicolas POZIN <nicolas@habx.fr>
2019-09-23 17:32:00 +0200

    reorganize - blob detector function put in a class of the blob_detector module

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 import numpy as np
 import time
 
-from blob_detector import Pixel, Blob
+from blob_detector import Pixel, Blob, BlobDetector
 from image_generator import generate_image
 
 if __name__ == '__main__':
 
     # generate synthetic image
+    # image = generate_image()
+
+    from PIL import Image
+
     image = generate_image()
+    img_tmp = Image.fromarray(np.uint8(image * 255), 'L')
+    img_tmp.save('blobs.tif')
 
     # time measure
     to = time.time()
 
-    # detect every blobs in the image
-    list_blobs = []
-    non_zeros = np.where(image == 1)
-    if not non_zeros:
-        print("no blob in the image")
-    else:
-        non_zero_pixels = list(
-            zip(non_zeros[0], non_zeros[1]))  # list of tuples with non zeros pixel coords
-        for p, pix in enumerate(non_zero_pixels):
-            pixel = Pixel(x=pix[0], y=pix[1], image=image)
-            if pixel.value != 1:
-                continue
-            blob = Blob(image, index=p + 2, seed_pixel=pixel, area=0)
-            blob.grow(pixel)
-            if blob.area > 400:
-                list_blobs.append(blob)
+    blob_detector = BlobDetector(image_path='blobs.tif', blob_size=400, blob_intensity=255)
+    list_detected_blobs = blob_detector.apply()
 
-    print("number of blobs detected ", len(list_blobs))
-    print("process time", time.time() - to)
+    print("blob detection process time", time.time() - to)
+
